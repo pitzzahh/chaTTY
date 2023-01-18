67f7fdbc3d37987edf0700610a300aa8621d2464
pitzzahh <pitzzahh@gmail.com>
2023-01-18 12:42:49 +0800

    add: exception on method signature

--- src/main/java/io/github/pitzzahh/chatbot/Launcher.java
+++ src/main/java/io/github/pitzzahh/chatbot/Launcher.java
@@ -1,9 +1,10 @@
 package io.github.pitzzahh.chatbot;
 
 import io.github.pitzzahh.chatbot.application.ChatBot;
+import java.io.IOException;
 
 public class Launcher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ChatBot.main(args);
     }
 }
